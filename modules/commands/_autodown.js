0b63f94448cd674a4156b7178d6108a18a249f11
R3B3L N4ZR9L <142940673+nazrul017@users.noreply.github.com>
2024-03-20 19:35:18 +0600

    Update _autodown.js

--- modules/commands/_autodown.js
+++ modules/commands/_autodown.js
@@ -3,8 +3,7 @@ module.exports.config = {
   version: "1.0.",
   hasPermssion: 0,
   credits: "nazrul",
-  usePrefix: false,
-  description: "All  Video Downloader",
+  description: "All Video Downloader",
   commandCategory: "Downloader",
   usages: "[video link]",
   cooldowns: 0
@@ -12,39 +11,39 @@ module.exports.config = {
 
 module.exports.handleEvent = async function ({ api, event, client, __GLOBAL }) {
   const axios = require('axios');
-const fs = require('fs-extra');
-let shaon = event.body ? event.body : '';
+  const fs = require('fs-extra');
+  let dipto = event.body ? event.body : '';
   try {
-if (shaon.startsWith('https://vt.tiktok.com') ||
-    shaon.startsWith("https://vm.tiktok.com") ||
-    shaon.startsWith('https://www.facebook.com') || 
-    shaon.startsWith('https://fb.watch')||
-    shaon.startsWith('https://www.instagram.com/')|| shaon.startsWith('https://youtu.be/') || shaon.startsWith('https://youtube.com/')){
-  api.sendMessage("downloading video, please wait...", event.threadID, event.messageID);
-  if (!shaon) {
-    api.sendMessage("please put a valid video link", event.threadID, event.messageID);
-    return;
+    if (dipto.startsWith('https://vt.tiktok.com') || dipto.startsWith('https://www.facebook.com') || dipto.startsWith('https://www.instagram.com/') || dipto.startsWith('https://youtu.be/') || dipto.startsWith('https://youtube.com/') || dipto.startsWith('https://x.com/') || dipto.startsWith('https://twitter.com/') || dipto.startsWith('https://vm.tiktok.com') || dipto.startsWith('https://fb.watch')) {
+      api.sendMessage("- W8 কর ডাউনলোড করতাছি 😒", event.threadID, event.messageID);
+      if (!dipto) {
+        api.sendMessage("please put a valid video link", event.threadID, event.messageID);
+        return;
+      }
+      const path = __dirname + `/cache/diptoo.mp4`;
+      const aa = await axios.get(`https://noobs-apihouse.onrender.com/dipto/aldl?url=${encodeURIComponent(dipto)}`);
+      const bb = aa.data;
+      const vid = (await axios.get(bb.result, { responseType: "arraybuffer" })).data;
+      fs.writeFileSync(path, Buffer.from(vid, 'utf-8'));
+      api.sendMessage({
+        body: `${bb.cp}`,
+        attachment: fs.createReadStream(path)
+      }, event.threadID, () => fs.unlinkSync(path), event.messageID)
     }
-    const path = __dirname + `/cache/shaonn.mp4`;
-    const aa = await axios.get(`https://d1pt0-all.onrender.com/xnxx?url=${encodeURI(shaon)}`);
-   const bb = aa.data;
-    const vid = (await axios.get(bb.result, { responseType: "arraybuffer", })).data;
-    fs.writeFileSync(path, Buffer.from(vid, 'utf-8'));
-    api.sendMessage({
-      body: `${bb.cp}`,
-      attachment: fs.createReadStream(path) }, event.threadID, () => fs.unlinkSync(path), event.messageID)}
-if (shaon.startsWith('https://i.imgur.com')){
-  const shaon3 = shaon.substring(shaon.lastIndexOf('.'));
-  const response = await axios.get(shaon, { responseType: 'arraybuffer' });
-const filename = __dirname + `/cache/shaon${shaon3}`;
-    fs.writeFileSync(filename, Buffer.from(response.data, 'binary'));
-    api.sendMessage({body: `Downloaded from link- ✅Title:${data.title}.\n✅Play Count: ${data.play_count}.\n✅Comment Count: ${data.comment_count}.\n✅Share Count: ${data.share_count}.\n✅Download Count: ${data.download_count}`,attachment: fs.createReadStream(filename)},event.threadID,
-  () => fs.unlinkSync(filename),event.messageID)
-}
-} catch (e) {
-api.sendMessage(`${e}`, event.threadID, event.messageID);
-  };
+    if (dipto.startsWith('https://i.imgur.com')) {
+      const dipto3 = dipto.substring(dipto.lastIndexOf('.'));
+      const response = await axios.get(dipto, { responseType: 'arraybuffer' });
+      const filename = __dirname + `/cache/dipto${dipto3}`;
+      fs.writeFileSync(filename, Buffer.from(response.data, 'binary'));
+      api.sendMessage({
+        body: `Downloaded from link`,
+        attachment: fs.createReadStream(filename)
+      }, event.threadID, () => fs.unlinkSync(filename), event.messageID)
+    }
+  } catch (e) {
+    api.sendMessage(`${e}`, event.threadID, event.messageID);
+  }
 };
-module.exports.run = function({ api, event, client, __GLOBAL }) {
+exports.run = function ({ api, event, client, __GLOBAL }) {
 
-}
+  }
