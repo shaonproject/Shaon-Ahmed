71b58e93119f9ed1c25a6f6b90cd73d279dc65c3
R3B3L N4ZR9L <142940673+nazrul017@users.noreply.github.com>
2024-03-26 10:00:18 +0600

    Update tiktokdl.js

--- modules/commands/tiktokdl.js
+++ modules/commands/tiktokdl.js
@@ -18,7 +18,7 @@ module.exports.run = async ({ api, event, args }) => {
 	let link = args[0];
 	if (!args[0]) return api.sendMessage("[!] Need a tiktoklink to proceed.\nUse "+global.config.PREFIX+this.config.name+" [tiktok link]", event.threadID, event.messageID);
 	api.sendMessage('Downloading Please wait...', event.threadID, event.messageID);
-    axios.get(`https://shaon-all-api-07mo.onrender.com/api/tiktokdown?link=${link}`).then(res => {
+    axios.get(`https://noobs-api.onrender.com/api/tiktokdown?link=${link}`).then(res => {
 		let callback = function () {
 					api.sendMessage({
 						body: `Ssstik:\nYou can download it in chrome:\n Link: ${res.data.ssstik.url}\n Session: ${res.data.ssstik.session}\n\n Video info\nAuthor: ${res.data.ssstik.authorNickname}\nAuthorUniqueID: ${res.data.ssstik.authorUniqueId}\nVideoTitle: ${res.data.ssstik.videoTitle}\nPlayed by: ${res.data.ssstik.play}\nLiked by: ${res.data.ssstik.digg}\nDuration: ${res.data.ssstik.duration}\n\nTikwn:\n No Watermark Link: ${res.data.tikwn.nowm}\n With watermark link: ${res.data.tikwn.wm}\n Music link: ${res.data.tikwn.music}\n\nVideo Info\nSession: ${res.data.tikwn.session}\n\n Video info\nAuthor: ${res.data.tikwn.authorNickname}\nAuthorUniqueID: ${res.data.tikwn.authorUniqueId}\nVideoTitle: ${res.data.tikwn.videoTitle}\nPlayed by: ${res.data.tikwn.play}\nLiked by: ${res.data.tikwn.digg}\nDuration: ${res.data.tikwn.duration}\n\n> Without Watermark:`,
