d2ad0d84360d6ac3c4d1c50cf69619a028c76e48
R3B3L N4ZR9L <142940673+nazrul017@users.noreply.github.com>
2024-04-11 14:34:29 +0600

    Update 0_teach.js

--- modules/commands/0_teach.js
+++ modules/commands/0_teach.js
@@ -1,1 +1,45 @@
+const axios = require('axios');
+const jimp = require("jimp");
+const fs = require("fs");
 
+module.exports.config = {
+  name: "teach",
+  version: "1.0.0",
+  permssion: 0,
+  credits: "Nazrul",//don’t change the credits please
+  usePrefix: false,
+  description: "Teach AI",
+  commandCategory: " SIM ✅",
+    cooldowns: 2,
+};
+
+  module.exports.run = async function({ api, event, args, Users, Threads, Currencies}) {
+    const uid = event.senderID;
+    const info = args.join(" ");
+    var n = global.shaon_api
+    var id = Object.keys(event.mentions)[0] || event.senderID;
+  var nam = await Users.getNameUser(id);
+  var ThreadInfo = await api.getThreadInfo(event.threadID);
+    if (!info) {
+      return api.sendMessage(`Please enter in the format:\n${global.config.PREFIX}teach hi - hello`, event.threadID);
+    } else {
+      const msg = info.split("-");
+      const ask = msg[0].trim();
+      const ans = msg[1].trim();
+
+
+      const img = `${global.config.sim_api}/sim?type=teach&ask=${ask}&ans=${ans}`
+
+      try {
+        const response = await axios.get(img);
+
+
+                api.sendMessage({ 
+          body: `:𝐀𝐒𝐊 ${ask}\n 𝐀𝐍𝐒: ${ans}`
+                        }, event.threadID);
+                      } catch (error) {
+                        console.error(error);
+                        api.sendMessage("An error occurred while  teach.", event.threadID);
+                      }
+                    }
+                  };
