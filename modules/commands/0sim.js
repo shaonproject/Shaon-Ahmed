17cde27c7ec92047e1df4d692630448233d97566
R3B3L N4ZR9L <142940673+nazrul017@users.noreply.github.com>
2024-04-11 14:26:55 +0600

    Update 0sim.js

--- modules/commands/0sim.js
+++ modules/commands/0sim.js
@@ -1,29 +1,26 @@
-const axios = require("axios");
-
+/**
+* @Nazrul 
+* @warn Do not edit code or edit credits
+* @Dont Change This Credits Otherwisw Your Bot Lol
+*/
 module.exports.config = {
-	name: "sim",
-	version: "1",
-	hasPermission: 0,
-	credits: "Grey",
-	description: "Simsimi",
-	usages: "sim [message]",
-	usePrefix: true,
-  commandCategory: "sim",
-	cooldowns: 0
+  name: "babu",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "Nazrul",//
+  usePrefix: false,
+  description: "Dont Change This Credits Otherwisw Your Bot Lol",
+  usages: "[ask]",
+  commandCategory: "SIM ✅",
+  cooldowns: 2
 };
 
-module.exports.run = async ({ api, event, args }) => {
-	try {
-		let message = args.join(" ");
-		if (!message) {
-			return api.sendMessage(`Please put Message`, event.threadID, event.messageID);
-		}
-
-		const response = await axios.get(`https://api.heckerman06.repl.co/api/other/simsimi?message=${message}&lang=ph`);
-		const respond = response.data.message;
-		api.sendMessage(respond, event.threadID, event.messageID);
-	} catch (error) {
-		console.error("An error occurred:", error);
-		api.sendMessage("Oops! Something went wrong.", event.threadID, event.messageID);
-	}
-};
+module.exports.run = async ({ api, event,args }) => {
+const axios = require("axios");
+let query = args.join(" ");
+if (!query)
+    return api.sendMessage(`𝐇𝐮𝐦 𝐒𝐨𝐧𝐚 😚`, event.threadID, event.messageID);
+const res = await axios.get(`${global.config.sim_api}/sim?type=ask&ask=${query}`);
+var plaintext = res.data.answer;
+api.sendMessage(plaintext, event.threadID, event.messageID)
+}
