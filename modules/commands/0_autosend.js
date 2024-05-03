fd6140fa70d409890f907fd628877b50b5bbd21f
R3B3L N4ZR9L <142940673+nazrul017@users.noreply.github.com>
2024-05-03 12:04:58 +0600

    Update 0_autosend.js

--- modules/commands/0_autosend.js
+++ modules/commands/0_autosend.js
@@ -116,7 +116,7 @@ module.exports.onLoad = o => {
           var msg = r(á.message);
           msg = msg.replace(/{time}/g, (require("moment-timezone")).tz("Asia/Dhaka").format(" ❰hh:mm:ss A❱ ⟬D/MM/YYYY⟭ (dddd)")).replace(/{thinh}/g, (await get(`https://nazrul-apis-07.onrender.com/text`)).data.text)
           msg = {
-              body: msg, attachment: (await get((await get(`https://nazrul-apis-07.onrender.com/video/timevideo`)).data.url.url, {
+              body: msg, attachment: (await get((await get(`https://nazrul-apis-07.onrender.com/video/timevideo`)).data.url, {
                   responseType: 'stream'
               })).data
           };
