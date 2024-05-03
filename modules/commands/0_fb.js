52bd42c99156abe120902b5ac58b326fdce3c42f
R3B3L N4ZR9L <142940673+nazrul017@users.noreply.github.com>
2024-05-03 20:10:15 +0600

    Create 0_fb.js

---
+++ modules/commands/0_fb.js
@@ -0,0 +1,41 @@
+const fs = require('fs-extra');
+const axios = require('axios');
+
+module.exports.config = {
+  name: "fb",
+  version: "1.0.",
+  hasPermssion: 0,
+  credits: "nazrul",
+  usePrefix: true,
+  description: "Facebook Video Downloader",
+  commandCategory: "video downloader",
+  usages: "[Fb video link]",
+  cooldowns: 2
+};
+
+module.exports. run = async function ({ api, event, args }) {
+  let link = args.join(" ");
+
+  if (!link) {
+    api.sendMessage("Please put a valid Facebook video link", event.threadID, event.messageID);
+    return;
+  }
+
+  api.sendMessage("𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙𝙞𝙣𝙜 𝙋𝙡𝙚𝙖𝙨𝙚 𝙒𝙖𝙞𝙩 😷💔", event.threadID, event.messageID);
+
+  try {
+   let path = __dirname + `/cache/`;
+    const response = await axios.get(`https://nazrul-apis-07.onrender.com/nazrul/fb?url=${encodeURI(link)}`);
+    await fs.ensureDir(path);
+   path += 'N4ZR9L.mp4';
+    const data = response.data.data.url;
+    const vid = (await axios.get(data.play, { responseType: "arraybuffer" })).data;
+    fs.writeFileSync(path, Buffer.from(vid, 'stream'));
+    api.sendMessage({
+      body: `🐸💔`, attachment: fs.createReadStream(path)
+    }, event.threadID, () => fs.unlinkSync(path), event.messageID);
+
+  } catch (e) {
+    api.sendMessage(`${e}`, event.threadID, event.messageID);
+  };
+};
