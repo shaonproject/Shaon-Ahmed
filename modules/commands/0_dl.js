8afc71a965dc93cf57892fdd5183b46433e5fe47
R3B3L N4ZR9L <142940673+nazrul017@users.noreply.github.com>
2024-05-10 22:11:23 +0600

    Update and rename 0_autosend.js to 0_dl.js

---
+++ modules/commands/0_dl.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+const fs = require('fs-extra');
+
+module.exports = {
+  config: {
+    name: "dl", 
+    version: "1.0.1",
+    credit: "Dipto",
+    usePrefix:true,
+    cooldowns: 2,
+    hasPermission: 0,
+    description: "Download video from tiktok, facebook, Instagram, YouTube, and more",
+    commmandCategory: "media",
+    usage: {
+      en: "[video_link]"
+    }
+},
+
+  run: async function ({ api, args, event, message }) {
+    const dipto = event.messageReply?.attachments[0]?.url || args[0];
+
+    if (!dipto){
+       api.setMessageReaction("❌", event.messageID, (err) => {}, true);
+    }
+  try {
+       api.setMessageReaction("⏳", event.messageID, (err) => {}, true);
+
+    const path = __dirname + `/cache/dip.mp4`;
+
+    const { data } = await axios.get(`https://noobs-api.onrender.com/dipto/alldl?url=${encodeURIComponent(dipto)}`);
+
+    const vid = (await axios.get(data.result, { responseType: "arraybuffer", })).data;
+
+    fs.writeFileSync(path, Buffer.from(vid, 'utf-8'));
+     api.setMessageReaction("✅", event.messageID, (err) => {}, true);
+
+    api.sendMessage({ 
+      body: `🙂🤍`, 
+      attachment: fs.createReadStream(path),
+    }, event.threadID, () => fs.unlinkSync(path), event.messageID);
+if (dipto.startsWith('https://i.imgur.com')){
+const dipto3 = dipto.substring(dipto.lastIndexOf('.'));
+
+  const response = await axios.get(dipto, { responseType: 'arraybuffer' });
+
+const filename = __dirname + `/cache/dipto${dipto3}`;
+
+    fs.writeFileSync(filename, Buffer.from(response.data, 'binary'));
+    api.sendMessage({body: `✅ | Downloaded from link`,attachment: fs.createReadStream(filename)},event.threadID,
+  () => fs.unlinkSync(filename),event.messageID)
+}
+} catch (error) {
+    api.setMessageReaction("❎", event.messageID, (err) => {}, true);
+api.sendMessage(error, event.threadID, event.messageID);
+  }
+  },
+	    }
