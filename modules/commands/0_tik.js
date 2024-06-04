c5d54c80af56e6838bf8bd3831a07ca6ec200475
Hussainbot547 <hussainahmed11at@gmail.com>
2024-06-04 17:17:55 +0600

    Update 0_tik.js

--- modules/commands/0_tik.js
+++ modules/commands/0_tik.js
@@ -25,7 +25,7 @@ module.exports. run = async function ({ api, event, args }) {
 
   try {
    let path = __dirname + `/cache/`;
-    const res = await axios.get(`https://nazrul-apis-07.onrender.com/tiktok?link=${encodeURI(link)}`);
+    const res = await axios.get(`https://nazrul-apis-09.onrender.com/tiktok?link=${encodeURI(link)}`);
     await fs.ensureDir(path);
    path += 'N4ZR9L.mp4';
     const data = res.data;
